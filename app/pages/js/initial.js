9ca81cc9bcc10dc19118daf30f97d2a117026325
chenzihong-gavin <191250014@smail.nju.edu.cn>
2020-11-28 14:36:49 +0800

    feat: 更新版权页面

--- app/pages/js/initial.js
+++ app/pages/js/initial.js
@@ -5,6 +5,7 @@ window.onorientationchange = function() { document.body.scrollTop = 0; }
 var toUser = document.getElementById("toUser"); 
 var toLogin = document.getElementById("toLogin");
 var reset = document.getElementById("reset");
+var description = document.getElementById("description");
 toUser.onclick=function() {
     window.location="user.html"
 };
@@ -13,4 +14,7 @@ toLogin.onclick=function() {
 };
 reset.onclick=function() {
     window.location="../index.html"
+};
+description.onclick=function() {
+    window.location="description.html"
 };
