2c43b36becfdd4401364a8fadf002f7298bda13f
chenzihong-gavin <191250014@smail.nju.edu.cn>
2020-11-28 22:16:33 +0800

    feat: 完成语音识别

--- app/pages/js/user.js
+++ app/pages/js/user.js
@@ -20,6 +20,9 @@ function welcome() {
         x.innerHTML = "快快戴上口罩，不然不许和我聊天";//改变内容
         talk("快快戴上口罩，不然不许和我聊天")
     }, 15000);
+    setTimeout(function () {
+        x.innerHTML = "";//改变内容
+    }, 18000);
 }
 welcome();
 
@@ -126,13 +129,58 @@ checkButton.onclick = async function(){
     const result = await runMaskDetection();
     if(MaskDetectionResult == "no mask"){
         text.innerText = "没有口罩，说出 “重新检测”后重新检测";
+        talk("没有口罩，说出 “重新检测”后重新检测");
         const restlt = runVoiceDetection();
         if(VoiceDetectionResult == "重新检测。"){
             const result = await runMaskDetection();
-            text.innerText = "检测到口罩"
+            text.innerText = "检测到口罩";
+            talk("检测到口罩");
+            
         }
     }
     if(MaskDetectionResult == "have mask"){
-        text.innerText = "请通过"
+        text.innerText = "请通过";
+        talk("请通过")
     }
 }
+
+//语音识别
+var AipSpeechClient = require("baidu-aip-sdk").speech;
+
+// 设置APPID/AK/SK
+var APP_ID = "20385348";
+var API_KEY = "8G9EPeh6Rv58KEPgKO6Uk4Ka";
+var SECRET_KEY = "ZypEqqbXtq1n99AYv5ChSNFmTfqucyLM";
+
+// 新建一个对象，建议只保存一个对象调用服务接口
+var client = new AipSpeechClient(APP_ID, API_KEY, SECRET_KEY);
+
+var HttpClient = require("baidu-aip-sdk").HttpClient;
+
+// 设置request库的一些参数，例如代理服务地址，超时时间等
+// request参数请参考 https://github.com/request/request#requestoptions-callback
+HttpClient.setRequestOptions({timeout: 5000});
+
+// 也可以设置拦截每次请求（设置拦截后，调用的setRequestOptions设置的参数将不生效）,
+// 可以按需修改request参数（无论是否修改，必须返回函数调用参数）
+// request参数请参考 https://github.com/request/request#requestoptions-callback
+HttpClient.setRequestInterceptor(function(requestOptions) {
+    // 查看参数
+    console.log(requestOptions)
+    // 修改参数
+    requestOptions.timeout = 5000;
+    // 返回参数
+    return requestOptions;
+});
+
+let fs = require('fs');
+let voice = fs.readFileSync('//data/home/chen/课件/project/No-touch-identification/app/pages/voice/20201128_214838.m4a');
+
+let voiceBuffer = new Buffer(voice);
+
+// 识别本地文件
+client.recognize(voiceBuffer, 'm4a', 16000).then(function (result) {
+    console.log('<recognize>: ' + JSON.stringify(result));
+}, function(err) {
+    console.log(err);
+});
