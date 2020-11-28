ed27db8738e24369ea06d1888a887eede9f3d1b6
dracoooooo <191250037@smail.nju.edu.cn>
2020-11-28 18:17:54 +0800

    feat: electron调用python

--- app/pages/js/user.js
+++ app/pages/js/user.js
@@ -5,7 +5,7 @@ window.onorientationchange = function() { document.body.scrollTop = 0; }
 function welcome() {
     x = document.getElementById("welcome");
     setTimeout(function () {
-        x.innerHTML = "欢迎访问脚本之家 www.jb51.net";//改变内容
+        x.innerHTML = "welcome";//改变内容
         x.style.color="#ff0000"; //改变样式
     }, 3000);
 }
@@ -29,3 +29,90 @@ axios.post('http://openapi.tuling123.com/openapi/api/v2', {
   }, (err) => {
     console.log(err);
   });
+
+
+
+const {PythonShell}  = require("python-shell");
+
+let MaskOptions = {
+    mode: 'text',
+    pythonPath: 'C:\\Users\\DRACO\\Anaconda3\\python.exe',
+    pythonOptions: [],
+    scriptPath: 'C:\\Users\\DRACO\\Downloads\\Mask-detector-master\\Mask-detector-master\\',
+    args: []
+};
+
+let VoiceOptions = {
+    mode: 'text',
+    pythonPath: 'C:\\Users\\DRACO\\Anaconda3\\python.exe',
+    pythonOptions: [],
+    scriptPath: 'C:\\Users\\DRACO\\Downloads\\Mask-detector-master\\Mask-detector-master\\',
+    args: []
+};
+
+var MaskDetectionResult;
+var VoiceDetectionResult;
+var text = document.getElementById("title");
+function runMaskDetection(){
+    return new Promise(resolve => {
+        PythonShell.run(
+            "mask.py", MaskOptions, function (err, results) {
+                if (err) throw err;
+                console.log('finish');
+                console.log('results', results);
+                MaskDetectionResult = results;
+                resolve("ok");
+            }
+        )
+    })
+}
+
+function runVoiceDetection(){
+    return new Promise(resolve => {
+        PythonShell.run(
+            "Baidu_api.py", VoiceOptions, function (err, results) {
+                if (err) throw err;
+                console.log('finish');
+                console.log('results', results);
+                VoiceDetectionResult = results;
+                resolve("ok");
+            }
+        );
+    });
+}
+
+// async function check() {
+//     if(MaskDetectionResult[0] === "no mask"){
+//         text.innerText = "没有口罩，说出 “重新检测”后重新检测";
+//         const restlt = await runVoiceDetection();
+//         if(VoiceDetectionResult[0] === "重新检测。"){
+//             const result = runMaskDetection();
+//             text.innerText = "检测到口罩"
+//         }
+//     }
+//     if(MaskDetectionResult[0] === "have mask"){
+//         text.innerText = "请通过"
+//     }
+// }
+
+var checkButton = document.getElementById("checkmask");
+
+checkButton.onclick = async function(){
+    const result = await runMaskDetection();
+    if(MaskDetectionResult == "no mask"){
+        text.innerText = "没有口罩，说出 “重新检测”后重新检测";
+        const restlt = runVoiceDetection();
+        if(VoiceDetectionResult == "重新检测。"){
+            const result = await runMaskDetection();
+            text.innerText = "检测到口罩"
+        }
+    }
+    if(MaskDetectionResult == "have mask"){
+        text.innerText = "请通过"
+    }
+}
+
+// document.getElementById("checkmask").onclick = runMaskDetection()
+
+
+
